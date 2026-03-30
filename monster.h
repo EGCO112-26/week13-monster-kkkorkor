9a686836f82ad0db37f597878e06198db03d469f
kkkorkor <kwankaow2112@gmail.com>
2026-03-30 08:37:01 +0000

    next class

--- monster.h
+++ monster.h
@@ -14,8 +14,20 @@ private:
 public:
     void Attack(monster &);
           void heal();
-    monster(string, int,int);
+    monster(string = "NAH", int=10,int=1); //constructor
+
+    ~monster();
 };
 
+monster::monster(string n, int h, int p){
+    name=n;
+    hp>200? hp=200:hp=h;
+    potion>10? potion=10:potion=p;
 
+    cout<<"Monster "<<name<<" ma leaw luk jaa"<<endl;
+}
+
+monster::~monster(){
+    cout<<"Bye Bye~ "<<name<<" hp: "<<hp<<endl;
+}
 #endif /* monster_h */
