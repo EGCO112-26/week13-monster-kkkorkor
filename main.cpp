0b331ad8381779e215b072b10f2ed2a2eda7d362
kkkorkor <kwankaow2112@gmail.com>
2026-03-30 08:35:55 +0000

    next class

--- main.cpp
+++ main.cpp
@@ -10,6 +10,13 @@ using namespace std;
 #include "monster.h"
 
 int main(int argc, const char * argv[]) {
+    monster *p;
+    monster m1("KK",15,7);
+    monster m2("Fang"),m4("JJ",30);
+    monster x[5] = {{"Gojo",100},{"Lin",50}};
+    p=new monster("Lulu",10,50);
+    delete p; //destructor
+   // monster m3; ----> error
  
-    return 0;
+    return 0; //destructor x,m4,m2,m1
 }
